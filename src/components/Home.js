337054687b5f9373bc2218bbe8c643cb3657ee50
srburke <shayray601@gmail.com>
2023-07-12 18:53:46 -0500

    Fixing up home page
    Changed color scheme, and put products on front page

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,9 +6,6 @@ import { useEffect, useState } from 'react';
 import { collection, query, doc, getDocs, getDoc, where, addDoc } from "firebase/firestore";
 import { db, auth } from "../config/firebase";
 
-
-// import '.../'
-
 const Home = (props) => {
     const [products, setProducts] = useState([]);
 
@@ -38,9 +35,9 @@ const Home = (props) => {
                     <div className="home-bg"></div>
                     <div className="container-fluid" id="home-cont">
                         <div className='container' id="intro" >
-                            <div className='section' >
+                            <div className='section'>
                                 <div className="row">
-                                    <div class="col-lg-6" id="intro-title">
+                                    <div class="col-lg-6" id="intro-title" >
                                         <h1 className="text-lg-start text-md-center text-sm-center home-title">MEET YOUR <br />  COFFEE NEEDS</h1>
 
                                         <h3 className="text-lg-start" style={{ marginTop: "2rem", fontSize: "1.5rem" }}>By Awakening Your Senses, One Bag at a Time</h3>
@@ -48,27 +45,28 @@ const Home = (props) => {
                                         <button type="button" class="btn btn-primary" id="subscribe-btn" style={{ marginTop: "1rem", backgroundColor: "hsl(150, 55%, 25%)", borderColor: "hsl(150, 55%, 25%)", marginRight: "14rem" }}>Explore Now</button>
                                     </div>
 
-                                    <div class="col-lg-6" id="intro-photo">
-                                        <img src="./images/gluten-free-non-dairy-creamer.png" alt="Coffee Photo" id="introImg" />
+                                    <div className="container-fluid" id="intro-photo">
+
+
+                                        <div class="col-lg-6">
+                                            <img src="./images/homePhoto.png" alt="Coffee Photo" id="introImg" />
+                                        </div>
                                     </div>
 
                                 </div>
 
                                 <div className="row">
 
-
                                 </div>
                             </div>
 
                         </div>
 
-
-
                     </div>
                 </section>
 
-                <section>
-                    <h1 className="text-lg-start text-md-center text-sm-center home-title" style={{ color: "#ffffff" }}>Medium <br /> </h1>
+                <section style={{ marginBottom: "3em" }}>
+                    <h1 className="text-lg-start text-md-center text-sm-center type-title" style={{ color: "#ffffff" }}>Medium <br /> </h1>
                     <Coffee products={products} />
                 </section>
             </main>
