0b452123be7fbde4708c2e68166c1931b75de4c5
srburke <shayray601@gmail.com>
2023-08-10 20:06:06 -0500

    Updated home page
    Added product to light product page

--- src/components/Home.js
+++ src/components/Home.js
@@ -22,7 +22,7 @@ const Home = () => {
                                     <div class="col-lg-6 col-md-8" id="intro-title" >
                                         <h1 className="text-lg-start text-md-center text-sm-center home-title">MEET YOUR <br />  COFFEE NEEDS</h1>
 
-                                        <h3 className="text-lg-start" style={{ marginTop: "2rem", fontSize: "1.5rem" }}>By Awakening Your Senses, One Bag at a Time</h3>
+                                        <h3 className="text-lg-start" style={{ marginTop: "2rem", fontSize: "1.5rem", paddingBottom: "2rem" }}>Awaken Your Senses, One Bag at a Time</h3>
 
                                         <button type="button" class="btn btn-primary" id="subscribe-btn" style={{ marginTop: "1rem", backgroundColor: "hsl(150, 55%, 25%)", borderColor: "hsl(150, 55%, 25%)", marginRight: "14rem" }}>Explore Now</button>
                                     </div>
@@ -54,29 +54,42 @@ const Home = () => {
                     <div className="container-fluid" id="roast-cont">
                         <div className="container">
                             <div className="row justify-content-center align-text-center g-2">
-                                <div class="col-lg-6">
-
-                                    < h1 className="type-title text-lg-start text-md-center text-sm-center">Explore Our <br /> Roast Levels </h1>
-                                    <p>lorem</p>
+                                <div className="col-lg-5 col-md-12">
+                                    <h1 className="type-title text-lg-start text-md-start text-sm-center">Explore Our <br /> Roast Levels </h1>
+                                    <p className="para-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt distinctio earum repellat quaerat voluptatibus placeat nam, commodi optio pariatur est quia magnam eum harum corrupti dicta, aliquam sequi voluptate quas. Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt distinctio earum repellat quaerat voluptatibus placeat nam, commodi optio pariatur est quia magnam eum harum corrupti dicta, aliquam sequi voluptate quas.</p>
                                 </div>
 
 
-                                <div class="col">
-                                    <a href='/coffee-type/light' className="nav-link" id='link'>
-                                        <h1 className="text-lg-start text-md-center text-sm-center">Light <br /> </h1></a>
+                                <div className="col">
+                                    <div className="roast-container">
+                                        <button className="roast-button" id="roast-text">    <a href='/coffee-type/light' className="nav-link" >
+                                        </a>Light</button>
+
+
+
+                                        <img src="./images/banner-1.jpg" alt="Coffee Photo" id="banner" />
+
+                                    </div>
 
                                 </div>
 
-                                <div class="col">
-                                    <a href='/coffee-type/medium' className="nav-link" id='link'>
-                                        <h1 className="text-lg-start text-md-center text-sm-center">Medium <br /> </h1></a>
+                                <div className="col">
+                                    <div className="roast-container">
+                                        <button className="roast-button" id="roast-text">    <a href='/coffee-type/medium' className="nav-link" >
+                                        </a>Medium</button>
 
+                                        <img src="./images/banner-1.jpg" alt="Coffee Photo" id="banner" />
+                                    </div>
                                 </div>
 
 
-                                <div class="col">
-                                    <a href='/coffee-type/dark' className="nav-link" id='link'>
-                                        <h1 className="text-lg-start text-md-center text-sm-center">Dark<br /> </h1></a>
+                                <div className="col">
+                                    <div className="roast-container">
+                                        <button className="roast-button" id="roast-text">    <a href='/coffee-type/dark' className="nav-link" >
+                                        </a>Dark</button>
+
+                                        <img src="./images/banner-1.jpg" alt="Coffee Photo" id="banner" />
+                                    </div>
 
                                 </div>
 
