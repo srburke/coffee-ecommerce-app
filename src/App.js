c8b236840f883db91d2843c84224342c7587c86f
srburke <shayray601@gmail.com>
2023-11-06 19:55:08 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -50,7 +50,7 @@ function App() {
       <NavBar />
 
       <Routes>
-        <Route path='/home' element={<Home loggeduser={loggeduser} />} />
+        <Route path='/coffee-ecommerce-app' element={<Home loggeduser={loggeduser} />} />
         <Route path='/coffee-type/light' element={<Coffee type={'Light'} />} />
         <Route path='/coffee-type/medium' element={<Coffee type={'Medium'} />} />
         <Route path='/coffee-type/dark' element={<Coffee type={'Dark'} />} />
