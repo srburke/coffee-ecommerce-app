b4ef6df54f3cc3a052e02f1db4083bfe1a08ccbe
srburke <shayray601@gmail.com>
2023-11-07 01:56:24 -0600

    Update NavBar.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -30,7 +30,7 @@ const NavBar = (props) => {
                     <div className="collapse navbar-collapse flex-grow-1" id="collapsibleNavId">
                         <ul className="navbar-nav mx-auto">
                             <li className="nav-item">
-                                <Link to='/coffee-type/light' id="link" className="nav-link" aria-current="page">Home<span
+                                <Link to='/' id="link" className="nav-link" aria-current="page">Home<span
                                     className="visually-hidden">(current)</span></Link>
                             </li>
                             <li class="nav-item dropdown" id="link">
@@ -38,9 +38,9 @@ const NavBar = (props) => {
                                     Coffee Bags
                                 </a>
                                 <ul className="dropdown-menu">
-                                    <li><a href='/coffee-type/light' className="nav-link" id='link'>Light Roast</a></li>
-                                    <li><a href='/coffee-type/medium' className="nav-link" id="link">Medium Roast</a></li>
-                                    <li><a href='/coffee-type/dark' className="nav-link" id="link">Dark Roast</a></li>
+                                    <li><Link to='/coffee-type/light' className="nav-link" id='link'>Light Roast</Link></li>
+                                    <li><Link to='/coffee-type/medium' className="nav-link" id="link">Medium Roast</Link></li>
+                                    <li><Link to='/coffee-type/dark' className="nav-link" id="link">Dark Roast</Link></li>
                                 </ul>
                             </li>
 
